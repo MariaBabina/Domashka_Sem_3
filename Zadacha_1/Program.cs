a7ed863f7b67191e79595f081443fc76bacf02ae
Maria Babina <babina.mariyka@gmail.com>
2023-12-09 18:38:14 +0200

    Added Zadacha 1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, 
+//значения которых лежат в отрезке [20,90].Пример массив [10 21 14 93 23] => 2
+
+int [] array = new int [10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next (0,101);
+    System.Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array [i] >= 20 && array[i] <= 90)
+    {
+        count++;
+    } 
+}
+Console.WriteLine(count);
+
